5f45ba37bcd61972fbe668c4352febd24cc4f987
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-04-28 21:07:10 +0700

    repository

---
+++ src/main/java/com/softech/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.softech.spring5webapp.repositories;
+
+import com.softech.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
